30f61414ae4fe7473499d1e2b0e5e12c9a93dfd2
yifan <Yifan-Ethan>
2018-08-30 13:49:47 +0800

    Just a simple commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Press any key to continue..");
+            ConsoleKeyInfo name = Console.ReadKey();
+        }
+    }
+}
